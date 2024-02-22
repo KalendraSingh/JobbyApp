c394dbd4bbe0c51466fd0dd21e77ef2c75ef289d
KalendraSingh <sangamslecture@gmail.com>
2024-02-22 08:14:51 +0000

    register page updated

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css'
 
 import {Route, Switch, Redirect} from 'react-router-dom'
 import LoginForm from './components/LoginForm'
+import Register from './components/Register'
 import Home from './components/Home'
 import NotFound from './components/NotFound'
 import ProtectedRoute from './components/ProtectedRoute'
@@ -15,6 +16,7 @@ import AllJobs from './components/AllJobs'
 const App = () => (
   <Switch>
     <Route exact path="/login" component={LoginForm} />
+    <Route exact path="/register" component={Register} />
     <ProtectedRoute exact path="/" component={Home} />
     <ProtectedRoute exact path="/jobs" component={AllJobs} />
     <ProtectedRoute exact path="/jobs/:id" component={AboutJobItem} />
--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -7,19 +7,27 @@ const websiteLogoInForm =
   'https://assets.ccbp.in/frontend/react-js/logo-img.png'
 
 class LoginForm extends Component {
-  state = {username: '', password: '', showSubmitError: false, errorMsg: ''}
+  state = {
+    username: '',
+    email: '',
+    password: '',
+    showSubmitError: false,
+    errorMsg: '',
+  }
 
   onGetUsername = event => this.setState({username: event.target.value})
 
+  onGetEmail = event => this.setState({email: event.target.value})
+
   onGetPassword = event => this.setState({password: event.target.value})
 
-  onSubmitSuccess = jwtToken => {
-    const {history} = this.props
+  //   onSubmitSuccess = jwtToken => {
+  //     const {history} = this.props
 
-    Cookies.set('jwt_token', jwtToken, {expires: 30, path: '/'})
+  //     Cookies.set('jwt_token', jwtToken, {expires: 30, path: '/'})
 
-    history.replace('/')
-  }
+  //     history.replace('/')
+  //   }
 
   onSubmitFailure = errorMsg => {
     this.setState({showSubmitError: true, errorMsg})
@@ -27,9 +35,9 @@ class LoginForm extends Component {
 
   onSubmitLoginForm = async event => {
     event.preventDefault()
-    const {username, password} = this.state
-    const userDetails = {username, password}
-    const loginApiUrl = 'https://users-7c43.onrender.com/login'
+    const {username, email, password} = this.state
+    const userDetails = {username, email, password}
+    const loginApiUrl = 'https://users-7c43.onrender.com/users'
     const options = {
       method: 'POST',
       headers: {
@@ -40,14 +48,15 @@ class LoginForm extends Component {
     const response = await fetch(loginApiUrl, options)
     const data = await response.json()
     if (response.ok === true) {
-      this.onSubmitSuccess(data.jwt_token)
+      const {history} = this.props
+      history.replace('/login')
     } else {
       this.onSubmitFailure(data.error)
     }
   }
 
   render() {
-    const {username, password, showSubmitError, errorMsg} = this.state
+    const {username, email, password, showSubmitError, errorMsg} = this.state
     const jwtToken = Cookies.get('jwt_token')
     if (jwtToken !== undefined) {
       return <Redirect to="/" />
@@ -74,6 +83,18 @@ class LoginForm extends Component {
             id="username"
           />
           <br />
+          <label className="form-label" htmlFor="email">
+            EMAIL
+          </label>
+          <br />
+          <input
+            className="form-input"
+            type="text"
+            value={email}
+            onChange={this.onGetEmail}
+            placeholder="email"
+            id="email"
+          />
           <br />
           <label className="form-label" htmlFor="password">
             PASSWORD
@@ -90,14 +111,11 @@ class LoginForm extends Component {
           <br />
           <br />
           <button className="form-submit-button" type="submit">
-            Login
+            Register
           </button>
           <p style={{textAlign: 'center'}}>
-            <Link
-              to="/register"
-              style={{textDecoration: 'none', color: 'white'}}
-            >
-              Don't have account?
+            <Link to="/login" style={{textDecoration: 'none', color: 'white'}}>
+              Already have account?
             </Link>
           </p>
           {showSubmitError && <p className="error-message">*{errorMsg}</p>}
